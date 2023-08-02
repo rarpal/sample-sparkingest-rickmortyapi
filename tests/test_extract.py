af0ee8edf026fc528c69bebd1207555707a528ea
Ravi Palihena <Ravi.Palihena@emea.nttdata.com>
2023-08-02 22:48:45 +0100

    add test placeholders for all extract tests

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -4,3 +4,18 @@ def test_get_rest_api():
     l = get_rest_api("https://rickandmortyapi.com/api/character")
 
     assert l != None
+
+def test_extract_character():
+    pass
+    # assert existence of columns
+    # assert existence of rows
+
+def test_extract_episode():
+    pass
+    # assert existence of columns
+    # assert existence of rows
+
+def test_extract_location():
+    pass
+    # assert existence of columns
+    # assert existence of rows
