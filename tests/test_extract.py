2dfe3bbdc4f0d71b8601630596475ff635e3fc52
Ravi Palihena <Ravi.Palihena@emea.nttdata.com>
2023-08-02 22:55:20 +0100

    add failed tests for extract_data

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,4 +1,7 @@
 from common.client import get_rest_api
+import extract.character
+import extract.episode
+import extract.location
 
 def test_get_rest_api():
     l = get_rest_api("https://rickandmortyapi.com/api/character")
@@ -6,16 +9,16 @@ def test_get_rest_api():
     assert l != None
 
 def test_extract_character():
-    pass
+    assert None
     # assert existence of columns
     # assert existence of rows
 
 def test_extract_episode():
-    pass
+    assert None
     # assert existence of columns
     # assert existence of rows
 
 def test_extract_location():
-    pass
+    assert None
     # assert existence of columns
     # assert existence of rows
